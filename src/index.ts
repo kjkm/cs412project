b67661fc7db9545c61845182cb6e6ce294bc146c
Kieran <kimmurkj@plu.edu>
2022-04-26 13:12:53 -0700

    feat: hello world!

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
     const header = document.createElement("h1");
-    header.innerText = "Webpack❤️TS"
+    header.innerText = "Hello from TypeScript!";
   
     const body = document.querySelector("body");
     body.appendChild(header);
