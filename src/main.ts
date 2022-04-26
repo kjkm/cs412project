98daf455a65770e6aeaf3914bad1e88109704896
loganmargo <logan.margo@plu.edu>
2022-04-26 13:32:45 -0700

    feat: added a main ts file

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import {Scene, PerspectiveCamera, WebGLRenderer, Mesh, SphereGeometry, ShaderMaterial, MeshBasicMaterial, MeshPhongMaterial, PointLight, TorusGeometry, ConeGeometry, AmbientLight, DirectionalLight, PlaneGeometry, Color, LinearEncoding, } from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls.js';
+import * as THREE from 'three';
